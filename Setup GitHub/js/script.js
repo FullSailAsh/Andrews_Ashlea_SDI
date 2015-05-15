4370ca138364062cb07415f9ad0b1c7b969c5674
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-14 21:53:10 -0400

    add in equation based on input
    add in equation based on input

--- Setup GitHub/js/script.js
+++ Setup GitHub/js/script.js
@@ -2,3 +2,6 @@
 
 var width = prompt("We are calculation the area of a rectangle.\nPlease enter the width.");
 var height = prompt("Please enter the height.");
+
+var area = width * height; //calculates area based on width and height input
+console.log(area);
