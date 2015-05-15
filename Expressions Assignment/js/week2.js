7b457cc83660d98b31a990e4599e526ffb7de73f
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-15 00:47:14 -0400

    started expressions assignment
    started expressions assignment

--- Expressions Assignment/js/week2.js
+++ Expressions Assignment/js/week2.js
@@ -1,8 +1,15 @@
 /*
-Ashlea Andrews
-5/13/2015
-Section 01
-Expressions Assignment
+ Ashlea Andrews
+ 5/13/2015
+ Section 01
+ Expressions Worksheet
+
+alert("testing to see if connected");
  */
 
-//alert("Testing to see if connected.")
+//find out users name
+var name = prompt("Hey there, what's your name?");
+console.log(name);
+
+//reason for calculator
+alert("Well, hello " + name + ", I'm going to assume you are like many")
