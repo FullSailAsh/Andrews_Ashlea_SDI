b135d24c809367ac0dcaa2f34339c35a8f210d33
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-14 22:13:08 -0400

    create and test
    Created and tested code for Slice of Pie Part 1

--- Expressions_worksheet/expressions_worksheet.js
+++ Expressions_worksheet/expressions_worksheet.js
@@ -3,20 +3,41 @@
  5/13/2015
  Section 01
  Expressions Worksheet
- */
+
 
 //Dog Years
-//Sparky's age in dog years
 
+//Sparky's age in dog years
 var dogsAge = prompt("How old is Sparky?");
 
 //Multiply by seven to find dogs age in human years
-
 var humanYears = dogsAge * 7;
 
 //String it together for print
-
 var output = "Sparky is " + humanYears + " years old, which is " + dogsAge + " in dog years.";
 console.log(output);
 
+//convey answer to user
 alert(output);
+ */
+
+//Slice of Pie Part 1
+
+//find out how many people at party
+var numPeople = prompt("Hey! How many people are at your party?");
+
+//find out how many pizzas there are
+var numPizza = prompt("Cool! How many pizzas did you all order?");
+
+//find out how many slices per pizza there are
+var slicesPP = prompt("Wow! How many slices are there per pizza?");
+
+//compute number of slices of pizza total
+var totalSlices = slicesPP * numPizza;
+
+//compute how many slices per person
+var slicesPerPerson = totalSlices / numPeople;
+console.log(slicesPerPerson);
+
+//tell user how many slices per person
+alert("Holy smokes! Each person ate " + slicesPerPerson + " slices of pizza at the party!");
