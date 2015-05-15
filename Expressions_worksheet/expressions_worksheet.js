58dff2eff14c4a15d384c87e095543b08e88382f
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-14 20:00:26 -0400

    Fixed html page, completed Dog years
    Removed unnecessary text from HTML file.
    Completed, and tested Dog Years problem in worksheet.

--- Expressions_worksheet/expressions_worksheet.js
+++ Expressions_worksheet/expressions_worksheet.js
@@ -4,3 +4,14 @@
  Section 01
  Expressions Worksheet
  */
+
+//Dog Years
+//Sparky's age in dog years
+var dogsAge = 3;
+
+//Multiplying by 7 to find dogs age in "human years"
+var humanAge = dogsAge * 7;
+
+//Combining variables for output
+var sparkysAge = "Sparky is " + humanAge + " human years old, which is " + dogsAge + " in dog years.";
+console.log(sparkysAge);
