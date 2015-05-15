c5ab3273c01ef0e1784cbd335606fbaf18259756
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-14 21:41:35 -0400

    final fix dog years
    fixed and tested with different variables.

--- Expressions_worksheet/expressions_worksheet.js
+++ Expressions_worksheet/expressions_worksheet.js
@@ -13,5 +13,8 @@ var dogsAge = prompt("How old is Sparky?");
 //Multiply by seven to find dogs age in human years
 
 var humanYears = dogsAge * 7;
-console.log(humanYears);
 
+//String it together for print
+
+var output = "Sparky is " + humanYears + " years old, which is " + dogsAge + " in dog years.";
+console.log(output);
