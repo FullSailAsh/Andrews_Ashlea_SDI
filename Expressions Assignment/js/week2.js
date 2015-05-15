172d19b25c9d7e78cd0884e0d1d496ed38e5ac5b
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-15 09:51:31 -0400

    test first computation
    test first computation

--- Expressions Assignment/js/week2.js
+++ Expressions Assignment/js/week2.js
@@ -2,7 +2,7 @@
  Ashlea Andrews
  5/13/2015
  Section 01
- Expressions Worksheet
+ Expressions Assignment
 
 alert("testing to see if connected");
  */
@@ -12,4 +12,16 @@ var name = prompt("Hey there, what's your name?");
 console.log(name);
 
 //reason for calculator
-alert("Well, hello " + name + ", I'm going to assume you are like many")
+alert("Well, hello " + name + "! Let's figure out everything you would need for a Maine lobster dinner!");
+
+//how many people
+var numPeople = prompt("How many people would you be planning this dinner for?");
+
+//how many lobsters per person
+var lobsters = prompt("How many lobsters a piece do you think they'll each want?");
+
+//convert number of lobsters to pounds of lobster
+var lobsters *= 1.25;
+console.log(lobsters);
+
+//
