f7946c4261b83cf1d236936e32fbbfd5b999f653
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-19 11:58:21 -0400

    true/false conditional practice
    true/false conditional practice

---
+++ Andrews_Ashlea_Conditionals/js/conditionals.js
@@ -0,0 +1,17 @@
+/*
+ Ashlea Andrews
+ 5/18/2015
+ Section 01
+Conditionals Worksheet
+
+ alert("testing to see if connected");
+ */
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
