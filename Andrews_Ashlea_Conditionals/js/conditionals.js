9fb3296cca0ebf5b480a16e47ee5fc6e42665bcf
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-19 19:47:26 -0400

    practice else if
    practice else if

--- Andrews_Ashlea_Conditionals/js/conditionals.js
+++ Andrews_Ashlea_Conditionals/js/conditionals.js
@@ -7,19 +7,22 @@ Conditionals Worksheet
  alert("testing to see if connected");
 
 */
-var kidHeight = 52;
+var kidHeight = 46;
 var minHeight = 48;
+var parentHeight = 45
 
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 in in height
 // expression, not variable
-if(kidHeight > 48){
-    //code performed if condition is true
+if(kidHeight > minHeight){
+    //you can ride--code performed if condition is true
     console.log("You can ride the coaster!");
-}
-//if the condition is false
-else{
- console.log("Sorry kid, you've got some growing to do!")
+}else if(kidHeight > parentHeight){
 
-};
+  //only evaluated if first part of the code isn't true you can ride with a parent present
+ console.log("You can ride, but only if you have a parent present!")
+}else {
+// if the condition is false
+ console.log("Sorry kid, you've got some growing to do!")
+}
