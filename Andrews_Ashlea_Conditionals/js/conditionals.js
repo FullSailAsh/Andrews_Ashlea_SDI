36399aff88515a2cdbd57952212f1b24abf39178
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-20 15:09:06 -0400

    use of &&
    use of &&

--- Andrews_Ashlea_Conditionals/js/conditionals.js
+++ Andrews_Ashlea_Conditionals/js/conditionals.js
@@ -6,10 +6,10 @@ Conditionals Worksheet
 
  alert("testing to see if connected");
 
-*/
+
 var kidHeight = 46;
 var minHeight = 48;
-var parentHeight = 45
+var wparentHeight = 45
 
 
 //if the child is old enough, print to the console "you can ride!"
@@ -18,11 +18,26 @@ var parentHeight = 45
 if(kidHeight > minHeight){
     //you can ride--code performed if condition is true
     console.log("You can ride the coaster!");
-}else if(kidHeight > parentHeight){
+}else if(kidHeight > wparentHeight){
 
   //only evaluated if first part of the code isn't true you can ride with a parent present
  console.log("You can ride, but only if you have a parent present!")
 }else {
 // if the condition is false
  console.log("Sorry kid, you've got some growing to do!")
+}
+ */
+
+//logical operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+//if the price of the phone is less than our budget AND if our paycheck is more than 300
+if(iPhonePrice < budget && paycheck > 300){
+  console.log("We can buy the phone!");
+
+}else{
+  console.log("No phone for you!!");
 }
