eb9f6c5c8d4a9ebb6130f3bfa83619bc8798dc57
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-21 16:21:38 -0400

    fixed location
    fixed location

---
+++ Conditionals_Worksheet/js/Andrews_Ashlea_Conditionals_Worksheet.js
@@ -0,0 +1,36 @@
+/*
+Conditionals Worksheet
+Ashlea Andrews
+section 01
+
+
+//alert("testing connection");
+
+//Group 1:  Expressions with Conditionals
+//Celsius to Fahrenheit converter
+
+//find the temperature
+var temp = prompt("Hi there!  What's the temperature where you are?");
+
+//find if that temperature is celsius or fahrenheit
+var unit = prompt("Is that Celsius or Fahrenheit? \nPlease be sure your first letter is upper case.");
+
+//if unit is celsius, print to console degrees converted in Fahrenheit, if Fahrenheit, print conversion into celsius
+if(unit ==="Celsius"){
+    //convert to fahrenheit, print to console
+    console.log("The temperature is " + ((parseInt(temp) * 9 / 5) +32) + " degrees Fahreinheit");
+
+}else{
+    console.log("The temperature is " + (((parseInt(temp) - 32)) * 5 / 9) + " degrees Celsius");
+}
+ */
+
+//Group 2
+//Grade Letter Calculator
+
+//find out grade
+var grade = prompt("Hey there! What number grade did you receive in your course?");
+
+if(grade ===){
+
+}
