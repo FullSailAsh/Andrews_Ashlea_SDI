4e5c75f485c29063b1b198329d6da6b1f506e6e2
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-21 16:44:02 -0400

    tested and finalized group 2
    tested and finalized group 2

--- Conditionals_Worksheet/js/Andrews_Ashlea_Conditionals_Worksheet.js
+++ Conditionals_Worksheet/js/Andrews_Ashlea_Conditionals_Worksheet.js
@@ -23,7 +23,7 @@ if(unit ==="Celsius"){
 }else{
     console.log("The temperature is " + (((parseInt(temp) - 32)) * 5 / 9) + " degrees Celsius");
 }
- */
+
 
 //Group 2
 //Grade Letter Calculator
@@ -31,6 +31,32 @@ if(unit ==="Celsius"){
 //find out grade
 var grade = prompt("Hey there! What number grade did you receive in your course?");
 
-if(grade ===){
+//match number to letter grade, and repeat
+if(grade >=95){
+    console.log("You have a " + grade + "% which means you have earned an A+ in the class!");
+
+}else if(grade >= 90 && grade <= 94){
+    console.log("You have a " + grade + "% which means you have earned an A in the class!");
 
 }
+else if(grade >=85 && grade <= 89){
+    console.log("You have a " + grade + "% which means you have earned a B+ in the class!");
+
+}else if(grade >= 80 && grade <= 84){
+ console.log("You have a " + grade + "% which means you have earned a B in the class!");
+
+}else if(grade >= 76 && grade <= 79){
+    console.log("You have a " + grade + "% which means you have earned a C+ in the class!");
+
+}else if(grade >= 73 && grade <= 75){
+    console.log("You have a " + grade + "% which means you have earned a C in the class!");
+
+}else if(grade >= 70 && grade <= 72){
+    console.log("You have a " + grade + "% which means you have earned a D in the class!");
+
+}else if(grade >= 0 && grade <= 69) {
+    console.log("Yikes! You have a " + grade + "% which means you have earned an F in the class!");
+}
+
+    */
+
