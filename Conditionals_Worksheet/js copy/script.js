0934ad974518269017f29b098a261721688f62fa
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-21 12:27:02 -0400

    Created files, C and F calculator
    Created files, C and F calculator

---
+++ Conditionals_Worksheet/js copy/script.js
@@ -0,0 +1,9 @@
+//prompt and alert
+
+var width = prompt("We are calculation the area of a rectangle.\nPlease enter the width.");
+var height = prompt("Please enter the height.");
+
+var area = width * height; //calculates area based on width and height input
+//console.log(area);
+var result = "The area of your rectangle is " + area + " square feet."
+alert(result);
--- Conditionals_Worksheet/screencast practice.js
+++ Conditionals_Worksheet/screencast practice.js
@@ -26,18 +26,58 @@ if(kidHeight > minHeight){
 // if the condition is false
  console.log("Sorry kid, you've got some growing to do!")
 }
- */
+
 
 //logical operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget AND if our paycheck is more than 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
+ //^ Asks is the iphone price less than budget true AND is paycheck greater than 300 true??
   console.log("We can buy the phone!");
 
 }else{
   console.log("No phone for you!!");
 }
+
+
+//ternaries
+
+if(condition){
+ do if true;
+}else{
+ do if false;
+}
+
+//ternary equivalent:
+
+(condition) ? do if true : do if false
+
+ */
+
+//var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+/*if( gpa > 2.0){
+ console.log("You can graduate!");
+}else{
+ console.log("GPA is too low!");
+}*/
+//( gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low");
+
+var age = 11;
+var book;
+
+//If the child is under 10, they get Green Eggs and Ham, otherise they get The Time Machine
+/*if(age < 10){
+ book = "Green Eggs and Ham";
+}else{
+ book = "The Time Machine";
+}
+console.log(book);*/
+//same as (in ternary):
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book)
