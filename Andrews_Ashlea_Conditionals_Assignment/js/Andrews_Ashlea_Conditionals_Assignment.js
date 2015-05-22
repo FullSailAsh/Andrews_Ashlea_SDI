a7c0852cc4c5f6133b9cc95f9625e9e2d9fb2bda
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-21 22:54:53 -0400

    tested with varying inputs
    tested with varying inputs

--- Andrews_Ashlea_Conditionals_Assignment/js/Andrews_Ashlea_Conditionals_Assignment.js
+++ Andrews_Ashlea_Conditionals_Assignment/js/Andrews_Ashlea_Conditionals_Assignment.js
@@ -5,9 +5,13 @@ Ashlea Andrews
  Section 01
  */
 
+//It's approaching summer in Maine, you said to use inspiration with what we know.
+//With getting the boat and traps ready, there's one thing (other than school, predominantly on the mind..
+// Get ready...it's another lobster calculator ;)
+
 //alert("Testing connection");
 
-//It's approaching summer in Maine, get ready...it's another lobster calculator ;)
+
 
 
 //Goal of calculator:  Find out how many pounds of lobster are needed, dependent on number of people and what size lobsters.
@@ -51,3 +55,24 @@ if(lobSize ==="small" || lobSize ==="smallest" || lobSize ==="smallest lobsters"
 }if(landLubbers ==="Yes" || landLubbers ==="yes"){
  console.log("Don't forget about an alternative for your non-seafood eaters!");
 }
+
+//Tested
+//tested validation --worked on all prompts
+//input 5 people
+//with someone not having seafood --correct output in both place
+//2 lobsters per person
+//largest lobster option --correct end print (20 pounds needed)
+
+//Tested
+//tested validation --worked on all prompts
+//input 3 people
+//no seafood problems --skipped appropriate code
+//2 lobsters each
+//large lobster option --correct end print (9 pounds needed)
+
+//Tested
+//tested validation --worked on all prompts
+//input 7 people
+//with seafood aversion --correct print in both places
+//2 lobsters each
+//"smallest" lobster option --correct print out (17.5lbs needed)
