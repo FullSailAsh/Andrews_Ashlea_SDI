49f51a0a31eac159166035edb8f260a24ad75002
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-28 18:27:17 -0400

    Practice function structure
    Practice function structure

--- Functions practice/js/functionsPractice.js
+++ Functions practice/js/functionsPractice.js
@@ -1,1 +1,13 @@
-alert("Testing connection");
+//alert("Testing connection");
+
+function outputMSg(){
+    console.log("Hello!");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
