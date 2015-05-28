8b239e65d05dafc1804b1f24d7d576123ad889d1
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-28 19:50:53 -0400

    scope
    scope

--- Functions practice/js/functionsPractice.js
+++ Functions practice/js/functionsPractice.js
@@ -1,13 +1,22 @@
 //alert("Testing connection");
 
+var width = 5;
+//out of scope of function, scoped in larger doc
+
+
 function outputMSg(){
     console.log("Hello!");
 }
+//setting up the function
 
 function calcArea(){
     var width = 20;
+    //^ scoped to calcArea
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+
+calcArea();
+//invocation : "calling the function"
 
