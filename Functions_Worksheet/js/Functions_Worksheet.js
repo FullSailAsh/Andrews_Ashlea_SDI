23c18469d6be608f9370b1f545ded9d9e70e7b44
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-28 20:49:05 -0400

    Stung! Prompt weight
    Stung! Prompt weight

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -2,13 +2,13 @@
 Ashlea Andrews
 May 27, 2015
 Functions Worksheet
-*/
+
 
 //alert("This is in the js file");
 
 //Circumference
 //Calculate the circumference of a circle using anonymous function
-var radius = prompt("What is the radius of your circle?")
+var radius = prompt("What is the radius of your circle?") //find out radius of circle
 
 var calcC = function(r){        //defines the function
     var circumference = 2 * r * 3.14;
@@ -17,3 +17,10 @@ var calcC = function(r){        //defines the function
 var output = calcC(radius); //invoking the function
 
 console.log("The circumference of the circle is " + output); //printing results
+ */
+
+//Stung!
+//Calculate how many stings needed to kill an animal based on animals weight in pounds
+
+var weight = prompt("How much did the victim weigh? Please answer in pounds"); //find out how much the victim weighed
+
