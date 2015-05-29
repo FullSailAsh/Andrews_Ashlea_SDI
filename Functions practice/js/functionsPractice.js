ad6f4dbf4659829401f7c83cbd2334638bad35ac
Andrews_Ashlea_SDI <aandrews@fullsail.edu>
2015-05-28 22:13:53 -0400

    attempt normal function
    attempt normal function

--- Functions practice/js/functionsPractice.js
+++ Functions practice/js/functionsPractice.js
@@ -7,11 +7,9 @@ var width = 5;
 function outputMSg(){
     console.log("Hello!");
 }
-//setting up the function
 
 function calcArea(){
     var width = 20;
-    //^ scoped to calcArea
     var height = 30;
     var area = width * height;
     console.log(area);
@@ -23,11 +21,9 @@ calcArea();
 
 //argument and parameter
 
-calcArea(30, 20);
- // 30 and 20 are arguments
+calcArea(30, 20);  // 30 and 20 are arguments
 
-function(w, h){ // same as w=30 and h=20
-    //w and h are parameters, or "storage mins"
+function(w, h){ // same as w=30 and h=20 //w and h are parameters, or "storage mins"
     var area = w * h;
     console.log(area);
 }
